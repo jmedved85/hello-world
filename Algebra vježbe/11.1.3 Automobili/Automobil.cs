d15ee6693bb68d658ddfbf749340145a7ca9f447
Jess <jmedved85@gmail.com>
2021-01-11 13:49:18 +0100

    11.1.3 Automobili

---
+++ Algebra vježbe/11.1.3 Automobili/Automobil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace _11._1._3_Automobili
+{
+    class Automobil
+    {
+        private string marka;
+        private string model;
+        private int zapremina;
+
+        //public Automobil(string marka, string model, string zapremina)
+        //{
+        //    this.marka = Marka;
+        //    this.model = Model;
+        //    this.zapremina = Zapremina;
+        //}
+
+        public string Marka { get => marka; set => marka = value; }
+        public string Model { get => model; set => model = value; }
+        public int Zapremina { get => zapremina; set => zapremina = value; }
+
+        public static Automobil UnosAutomobila() // metoda za unos podataka o automobilu
+        {
+            Automobil auto = new Automobil(); // najprije formiranje objekta kojeg ćemo vraćati van sa svim unešenim svojstvima
+            Console.Write("Unesite marku automobila: ");
+            auto.Marka = Console.ReadLine(); 
+            Console.Write($"Unesite model automobila {auto.Marka}: ");
+            auto.Model = Console.ReadLine();
+            Console.Write($"Unesite zapreminu automobila {auto.Marka} {auto.Model}: ");
+            auto.Zapremina = int.Parse(Console.ReadLine());
+            return auto; // unešenog objekta iz metode
+        }    
+        
+
+    }
+}
---
+++ Algebra vježbe/11.1.3 Automobili/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._1._3_Automobili
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AUTOMOBILI");
+            List<Automobil> auti = new List<Automobil>(); // formiranje glavne liste
+            string unos = "".ToLower();
+            while (unos !="n") // dokle god nije unos n za izlaz
+            {
+                auti.Add(Automobil.UnosAutomobila()); // dodavanje automobila sa svojstvima (Marka, Model, Zapremina) u glavnu listu preko posebne metode za unos (UnosAutomobila())
+                Console.WriteLine("Želite li upisati novi automobil (d/n)?");
+                unos = Console.ReadLine();
+            }     
+            Console.WriteLine();
+            Console.WriteLine("Svi automobili");
+            foreach (var item in auti) // Ispis svih unešenih automobila (bez izlistane zapremine)
+            {
+                Console.WriteLine($"{item.Marka} {item.Model}");
+            }
+
+            // Upit za ispis modela velike zapremine
+            List<string> modeliVelikeZapremine = (from auto in auti // vade se objekti iz glavne liste
+                                                  orderby auto.Model // poredati po nazivu modela
+                                                  where auto.Zapremina > 1600 // gdje su objekti veći od 1600
+                                                  select auto.Model).ToList(); // izbacije se objekt po nazivu modela
+            Console.WriteLine();
+            Console.WriteLine("Pronađeni modeli velike zapremine su:");
+            foreach (string model in modeliVelikeZapremine)
+            {
+                Console.WriteLine(model); // ispis
+            }
+
+
+        }
+    }
+}
