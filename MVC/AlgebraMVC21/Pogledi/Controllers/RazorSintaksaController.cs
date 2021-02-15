6a6ac687f4971a7d7440406bbdc1fb670c12fc7a
Jess <jmedved85@gmail.com>
2021-02-15 19:45:26 +0100

    iz proručnika 15-02-21

---
+++ MVC/AlgebraMVC21/Pogledi/Controllers/RazorSintaksaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pogledi.Controllers
+{
+    public class RazorSintaksaController : Controller
+    {
+        // GET: RazorSintaksa
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult SwitchView()
+        {
+            return View();
+        }
+        public ViewResult ForView()
+        {
+            string[] voce = new string[] { "Jabuka", "Kruška", "Limun", "Banana" };
+            return View(voce);
+        }
+        public ViewResult WhileView()
+        {
+            return View();
+        }
+    }
+}
