29210a13ee1afad791ce8490462ebb066332b47e
Jess <jmedved85@gmail.com>
2021-02-17 17:41:51 +0100

    vježba iz priručnika 17-02-21

---
+++ MVC/AlgebraMVC21/3_1_vj/Controllers/KontekstController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _3_1_vj.Controllers
+{
+    public class KontekstController : Controller
+    {
+        // GET: Kontekst
+        public string Index()
+        {
+            string serverName = Server.MachineName;
+            string trenutniDatum = DateTime.Now.ToLongDateString();
+            string poruka = $"Trenutni datum je {trenutniDatum}, a računalo je {serverName}.";
+            return poruka;
+        }
+
+        public string
+    }
+}
